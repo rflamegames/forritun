2709a785698b0ace4c1f69559382fdcb86b0cdf0
rflamegames <40735780+rflamegames@users.noreply.github.com>
2020-09-03 14:10:45 +0000

    fixed algorithm

--- max_int.py
+++ max_int.py
@@ -1,4 +1,17 @@
+#User inputs  a series of numbers, when he inputs a negative number
+#the program stops and outputs the largest positive number.
+#We can use a for loop that repetedly asks the user for a number to
+#input and then we add that to the max_int var, once we do that again
+#we check if the number is positive, then we check if its larger than 
+#max_int if it is we make it the new max_int
 
-num_int = int(input("Input a number: "))    # Do not change this line
-# Fill in the missing code
+
+bool1 = True
+max_int = int(0)
+while bool1 == True:
+    num_int = int(input("Input a number: "))    # Do not change this line
+    if num_int <= 0:
+        bool1 = False
+    if max_int <= num_int:
+        max_int = num_int
 print("The maximum is", max_int)    # Do not change this line
